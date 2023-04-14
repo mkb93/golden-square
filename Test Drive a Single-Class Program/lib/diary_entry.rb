feefaa5ec6da5eb8d81b1c343f782cb5748cb520
marco belahouane <nfk18@hotmail.co.uk>
2023-04-14 13:50:21 +0100

    reseting diary entry for better practice

--- Test Drive a Single-Class Program/lib/diary_entry.rb
+++ Test Drive a Single-Class Program/lib/diary_entry.rb
@@ -1,23 +1,24 @@
 class DiaryEntry
     def initialize(title, contents) # title, contents are strings
-      @title = title
-      @contents = contents
+      # ...
     end
   
     def title
-      return @title
+      # Returns the title as a string
     end
   
     def contents
-      return @contents
+      # Returns the contents as a string
     end
   
     def count_words
-      return @contents.split(' ').length
+      # Returns the number of words in the contents as an integer
     end
   
-    def reading_time(wpm) 
-      return count_words / wpm 
+    def reading_time(wpm) # wpm is an integer representing the number of words the
+                          # user can read per minute
+      # Returns an integer representing an estimate of the reading time in minutes
+      # for the contents at the given wpm.
     end
   
     def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
