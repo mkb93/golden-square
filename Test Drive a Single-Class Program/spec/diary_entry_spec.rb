e2b92001c393a3270272d45cf1b0e432c260cfbb
marco belahouane <nfk18@hotmail.co.uk>
2023-04-14 10:51:22 +0100

    added a second test to rspec diary_entry

--- Test Drive a Single-Class Program/spec/diary_entry_spec.rb
+++ Test Drive a Single-Class Program/spec/diary_entry_spec.rb
@@ -1,5 +1,14 @@
 require './lib/diary_entry.rb'
 
 RSpec.describe DiaryEntry do
-    
+  it 'outputs the title of the variable' do
+    title = DiaryEntry.new('Love','it all began when we went to the moon the low lights were wonderous.')
+    result = title.title
+    expect(result).to eq 'Love' 
+  end
+  it 'outputs the contents of the variable' do
+    contents =  DiaryEntry.new('Love','it all began when we went to the moon the low lights were wonderous.')
+    result = contents.contents
+    expect(result).to eq 'it all began when we went to the moon the low lights were wonderous.'
+  end 
 end 
