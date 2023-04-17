3417290e135b2111cc5e3873d4d020baf73e5da7
marco belahouane <nfk18@hotmail.co.uk>
2023-04-17 12:12:01 +0100

    completed Task tracker and task compelte method

--- Design-a-single-class-program/lib/task_tracker.rb
+++ Design-a-single-class-program/lib/task_tracker.rb
@@ -1,18 +1,21 @@
 class TaskTracker 
-  def init
+  def initialize
     @task_array = []
   end
   
   def add_task (str)
-    @task_array = str
+    @task_array << str
   end
     
   def show_tasks
-    return @task_array
+    return @task_array.join(', ')
   end
   
-  def task_update(str, status)
-  
+  def task_complete(str)
+    fail "no tasks added" unless @task_array.length>0
+    index = @task_array.find_index(str)
+    @task_array.delete_at(index)
+    return show_tasks
   end
 end
   
--- Design-a-single-class-program/spec/task_tracker_spec.rb
+++ Design-a-single-class-program/spec/task_tracker_spec.rb
@@ -8,5 +8,27 @@ RSpec.describe TaskTracker do
       result = tasks.show_tasks
       expect(result).to eq 'buy a car'
     end
+    it "passes when add a task and return that one task in string format" do
+      tasks = TaskTracker.new
+      tasks.add_task('buy a car')
+      tasks.add_task('buy a car')
+      tasks.add_task('buy a car')
+      result = tasks.show_tasks
+      expect(result).to eq 'buy a car, buy a car, buy a car'
+    end
+  end
+  context "#task_complete" do
+    it "pass when list is updated removing one of the tasks" do
+    tasks = TaskTracker.new
+    tasks.add_task('buy a car')
+    tasks.add_task('buy a dress')
+    tasks.add_task('buy some fruit')
+    tasks.task_complete('buy a car')
+    expect(tasks.show_tasks()).to eq 'buy a dress, buy some fruit'
+    end
+    it "fail when list is empty and we try complete a task removing one of the tasks" do
+      tasks = TaskTracker.new
+      expect{tasks.task_complete('buy a car')}.to raise_error "no tasks added"
+    end
   end
 end
