04178057c59e596b3f4d86fecec7225dbb52c86f
marco belahouane <nfk18@hotmail.co.uk>
2023-04-20 11:37:47 +0100

    challenge in phase 3.1 completed

--- phase3/unit-testing-parent-child-classes/challenge/mocking_bites/spec/task_list_spec.rb
+++ phase3/unit-testing-parent-child-classes/challenge/mocking_bites/spec/task_list_spec.rb
@@ -10,6 +10,26 @@ RSpec.describe TaskList do
     task_list = TaskList.new
     expect(task_list.all_complete?).to eq false
   end
-
+  
+  describe '#all' do
+    it "returns both tracks" do 
+    task_list = TaskList.new
+    task1 = double :task, title: 'task1'
+    task2 = double :task, title: 'task2'
+    task_list.add(task1)
+    task_list.add(task2)
+    expect(task_list.all).to eq [task1, task2]
+    end
+  end
+  describe '#all_complete?' do
+  it "returns both tracks" do 
+    task_list = TaskList.new
+    task1 = double :task, title: 'task1', complete?: false
+    task2 = double :task, title: 'task2', complete?: false
+    task_list.add(task1)
+    task_list.add(task2)
+    expect(task_list.all_complete?).to eq false
+    end
+  end
   # Unit test `#all` and `#all_complete?` behaviour
 end
